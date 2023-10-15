44fec79de77403481e0e58f564cf7ba5c9ff90aa
Shakiba Sadat Vakili <122327036+shakiba-vakili@users.noreply.github.com>
2023-10-15 22:28:18 +0330

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # realeState
+# realeState
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <section className="h-wrapper">
+      <div className="h-container paddings innerWidth flexCenter">
+        <img src="./logo.png" alt="logo" width={100} />
+        <div className="flexCenter h-menu">
+          <a href="">Residencies</a>
+          <a href="">Our Value</a>
+          <a href="">Contact Us</a>
+          <a href="">GEt Started</a>
+          <button className="button">
+            <a href="">Contact</a>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
