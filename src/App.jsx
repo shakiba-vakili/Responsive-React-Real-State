27717894ad78bc0e75bf745b3fa2965bd6b5bcac
shakiba vakili <shakibvakili@gmail.com>
2023-10-17 08:59:12 +0330

    company

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import "./App.css";
+import Company from "./components/Company/Company"
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         <Header />
         <Hero />
       </div>
+      <Company/>
     </div>
   );
 }
---
+++ src/components/Company/Company.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Company.css";
+const Company = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default Company;
