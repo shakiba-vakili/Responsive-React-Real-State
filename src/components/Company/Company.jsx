69f6aa275991240b9bcfa8b0baca181a0c76feba
shakiba vakili <shakibvakili@gmail.com>
2023-10-17 10:01:42 +0330

    Residencies

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -4,7 +4,10 @@ const Company = () => {
   return (
     <section className="c-wrapper">
       <div className="paddings innerWidth flexCenter c-container">
-        <img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" />
+        <img src="./prologis.png" alt="" />
+        <img src="./tower.png" alt="" />
+        <img src="./equinix.png" alt="" />
+        <img src="./realty.png" alt="" />
       </div>
     </section>
   );
