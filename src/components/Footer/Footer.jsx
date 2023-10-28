4e577070f3961699d76a5ce858427efd4367abe6
shakiba vakili <shakibvakili@gmail.com>
2023-10-28 13:22:58 +0330

    footer section

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <section className="f-wrapper">
+      <div className="innerWidth paddings flex-center f-container">
+        <div className="flexColStart f-left">
+          <img src="./logo2.png" alt="" width={120} />
+          <span className="secondartText">
+            Lorem ipsum dolor sit amet.
+            <br />
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </span>
+        </div>
+        <div className="flexColStart f-right">
+          <span className="secondartText">Information</span>
+          <span className="secondartText">145 New York, FL 4578, USA</span>
+          <div className="flexCenter f-menu">
+            <span>Property</span>
+            <span>Services</span>
+            <span>Product</span>
+            <span>About Us</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Footer;
